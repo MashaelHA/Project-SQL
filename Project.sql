9d6fe447c13a60cab663ac6a2815233473e50536
Mashael Alasmari <59392815+MashaelHA@users.noreply.github.com>
2022-05-11 16:08:24 +0300

    Add files via upload

---
+++ Project.sql
@@ -0,0 +1,220 @@
+INSERT INTO Store_Branches VALUES (1,'Imported Food1', '100 Bleecker St, New York, NY 10013, USA ', 10 ,'7:30:00.0',
+'24:30:00.0') ;
+INSERT INTO Store_Branches VALUES (2,'Imported Food2', ' 5914 schaefer rd, Dearborn, mi 48126, usa ',6,'7:30:00.0',
+'24:30:00.0') ;
+INSERT INTO Store_Branches VALUES (3,'Imported Food3', ' (424) 206-1138509 Esplanade Redondo Beach,
+California(CA), 90277',5,'10:30:00.0', '21:30:00.0');
+
+INSERT INTO Jobs VALUES ( 1111,'Manger');
+INSERT INTO Jobs VALUES (2222,'Accountant');
+INSERT INTO Jobs VALUES (3333,' Shelves_dresser') ;
+INSERT INTO Jobs VALUES (4444,' Security');
+INSERT INTO Jobs VALUES (5555,' Delivery representative') ;
+INSERT INTO Jobs VALUES (6666,' Customer_Service') ;
+INSERT INTO Jobs VALUES (7777,' Warehouse_manager');
+INSERT INTO Jobs VALUES (8888, ' Chief_Execusive_Officer') ;
+
+INSERT INTO JobDetails VALUES (1111,1,1);
+INSERT INTO JobDetails VALUES (1111,1,2);
+INSERT INTO JobDetails VALUES (1111,1,3);
+INSERT INTO JobDetails VALUES (2222,2,1);
+INSERT INTO JobDetails VALUES (2222,2,2);
+INSERT INTO JobDetails VALUES (2222,1,3);
+INSERT INTO JobDetails VALUES (3333,2,1);
+INSERT INTO JobDetails VALUES (3333,1,2);
+INSERT INTO JobDetails VALUES (3333,1,3);
+INSERT INTO JobDetails VALUES (4444,1,1);
+INSERT INTO JobDetails VALUES (4444,1,2);
+INSERT INTO JobDetails VALUES (4444,1,3);
+INSERT INTO JobDetails VALUES (5555,1,1);
+INSERT INTO JobDetails VALUES (5555,1,2);
+INSERT INTO JobDetails VALUES (5555,1,3);
+
+INSERT INTO JobDetails VALUES (6666,1,1);
+INSERT INTO JobDetails VALUES (6666,1,2);
+INSERT INTO JobDetails VALUES (7777,1,1);
+INSERT INTO JobDetails VALUES (8888,1,1);
+
+INSERT INTO EmployeeDetails VALUES ('123843876', 'Sam', 'M', 'Zain',45, '+17698765', 'Johan1@gmail.com', 'male', ' 371
+7th Ave, New York, NY 10001',1111, 1) ;
+INSERT INTO EmployeeDetails VALUES ('163817306','Lyn','K','Lucas',35, '+12421791','Lynnn@gmail.com','male',' 55 East
+10th Street, New York, NY 10003, United States',2222,1);
+INSERT INTO EmployeeDetails VALUES ('712126944', 'Joyes','L', 'Dylan',30,'+11217381','JoyesDylan@gmail.com','male', '
+345 E 24th St, New York, NY 10010, USA',2222,1);
+INSERT INTO EmployeeDetails VALUES ('812315439', 'Edward', 'S', 'Elion',25, '+15834375', 'EdwardS55@gmail.com',
+'male', ' 110 W 3rd St, New York, NY 10012, USA',3333, 1);
+INSERT INTO EmployeeDetails VALUES ('213683224', 'Sara', 'W', 'Tem',22, '+11236722', 'SaraW@gmail.com', 'female', '
+120 E 12th St, New York, NY 10003, USA ',3333, 1);
+INSERT INTO EmployeeDetails VALUES ('122333553','Rain','J','Joes',37, '+11532643','Mias@gmail.com','male',' 80
+Washington Square E, New York, NY 10003, USA',4444,1);
+INSERT INTO EmployeeDetails VALUES ('121835624','Jolate','X','White',38,
+'+11638322','WhiteJolate@gmail.com','female', '60 East , michigan', 1111,2) ;
+INSERT INTO EmployeeDetails VALUES ('212621253','Jack','X','Sam',28, '+13146411','Jack1s@gmail.com','male','60
+North , michigan',2222,2) ;
+INSERT INTO EmployeeDetails VALUES ('336215399','Emma','R','Dylan',24,
+'+13226427','DylanEmmas@gmail.com','female','90North,michigan',2222,2) ;
+INSERT INTO EmployeeDetails VALUES ('123732637','Mia','W','Henry',33, '+11321453','Mias@gmail.com','female', '60
+East , michigan',3333,2) ;
+INSERT INTO EmployeeDetails VALUES ('327150524','Kai','M','Zain',36, '+12224423','KaiMzain@gmail.com','female','626
+W. Clark Rd.Moreno Valley, CA 92553',1111,3) ;
+INSERT INTO EmployeeDetails VALUES ('120442217','Naya','S','Henry',28, '+12212663','Niahs@gmail.com','female','
+Chestnut Rd. Carson, CA 90745',2222,3);
+INSERT INTO EmployeeDetails VALUES ('130122323','Sam','Z','Hani',29, '+13224643','SamHenry@gmail.com','male','131
+Griffin Ave.South San Francisco, CA 94080',3333,3);
+INSERT INTO EmployeeDetails VALUES ('19944333','Lisa','J','Samual',39, '+11592623','LisaLisa@gmail.com','female','78
+Lakewood Street Hanford, CA 93230',4444,3);
+INSERT INTO EmployeeDetails VALUES ('122111653','Eva','M','Aiden',21, '+11932743','Evaas@gmail.com','female','240
+Mercer St, New York, NY 10012, USA',6666, 1);
+INSERT INTO EmployeeDetails VALUES ('122618683','Lisa','S','Aiden',44,
+'+18832783','LisaSAiden@gmail.com','female','21 East , michigan',6666, 2);
+
+INSERT INTO EmployeeDetails VALUES ('122608677','Alex','R','Sam',27, '+18821116','TheAlex@gmail.com','male','26
+Washington Pl, New York, NY 10003, USA', 5555, 1);
+INSERT INTO EmployeeDetails VALUES
+('100433667','Aemilia','Z','White',34,'+18999916','itsAemilia@gmail.com','female','90North, michigan ',5555, 2);
+INSERT INTO EmployeeDetails VALUES ('122660555','John','L','Daniel',40,'+18824422','JohnLL@gmail.com','male','132
+Griffin Ave.South San Francisco, CA 94081', 5555, 3) ;
+INSERT INTO EmployeeDetails VALUES ('122069339','Darwied','M','John',39,
+'+19821642','Darwied@gmail.com','male','32 Washington Pl, New York, NY 10003, USA',7777, 1);
+INSERT INTO EmployeeDetails VALUES ('122660077','Alex','R','Sam',41,'+18821116','TheAlex@gmail.com','male','7
+Washington Pl, New York, NY 10003, USA', 8888, 1);
+INSERT INTO EmployeeDetails VALUES ('10900333','John','J','Zain',42, '+10590023','JohnJZain@gmail.com','male','28
+East , michigan',4444,2) ;
+
+INSERT INTO Salary VALUES (1111,'part_time_job', 8333.20 );
+INSERT INTO Salary VALUES (2222,'Full_time_job', 4666.64 );
+INSERT INTO Salary VALUES (3333,'Full_time_job', 3333.20 );
+INSERT INTO Salary VALUES (4444,'Full_time_job',3400.20 );
+INSERT INTO Salary VALUES (5555,'part_time_job', 3599.84 );
+INSERT INTO Salary VALUES (6666,'part_time_job', 4443.60 );
+INSERT INTO Salary VALUES (7777,'part_time_job', 6665.99);
+INSERT INTO Salary VALUES (8888,'part_time_job', 7133.12 );
+
+INSERT INTO Suppliers VALUES('SADAFCO',01230,'23241, Jeddah','126293370','SADAFCO.SA@SADAFCO.com');
+INSERT INTO Suppliers VALUES('Kingdom Dates',01231,'MMMR+2F','985468900','sales@kdc.sa') ;
+INSERT INTO Suppliers VALUES('FAZER FOOD SERVICES OY',01232,'Kaarnatie 4, 00410
+Helsinki','+35820555','forename@fazer.com') ;
+INSERT INTO Suppliers VALUES('GEMAK FOOD INDUSTRY MACHINERY CO',01233,'Manchester,M24
+5UJ','+01616270','sales@gemak.co.uk') ;
+INSERT INTO Suppliers VALUES('GOODY',01234,'X4FC+6W','971488090','info@goody.com.sa') ;
+
+INSERT INTO Suppliers VALUES('GULF CATERING FOOD FACTORY',01235,'JR7C+6G Al-
+Riyad','920009096','contactus@gccgff.com');
+
+INSERT INTO Suppliers VALUES('Access Skills Ltd',01236,'Birmingham, B15 1TH','121510216','info@accessskills.co.uk') ;
+INSERT INTO Suppliers VALUES('apetito',01237,'7CCV+GV','122753636','carehomes@apetito.co.uk') ;
+INSERT INTO Suppliers VALUES('Boots',01238,'8mn+SE','159494047','care@boots.co.uk');
+
+INSERT INTO Suppliers VALUES('ABC Bakery Supply',01239,'49 Gilbert St','715552222','carehomes@ABCbakery.co.uk') ;
+INSERT INTO Suppliers VALUES('Leka Trading',012310,'471 Serangoon Loop,
+Suite','555878787','contactus@LekaTrading.com');
+INSERT INTO Suppliers VALUES('American Food Distributor',012311,'Florida
+34668','278481010','info@AmericanFood.co.uk') ;
+
+INSERT INTO Products VALUES(1,'Dark Chocolate & Red Berries',22.38,1002) ;
+INSERT INTO Products VALUES(2,'Dark Chocolate & Peanut Butter',22.38,2468);
+INSERT INTO Products VALUES(3,'Apple Chia Crumble',22.38,4699);
+INSERT INTO Products VALUES(4,'Dark Chocolate Macaroon',22.38,200);
+INSERT INTO Products VALUES(5,'Double Chocolate Chunk',22.38,15);
+INSERT INTO Products VALUES(6,'Al walimah Indian sella basmati rice',67.95,579) ;
+INSERT INTO Products VALUES(7,'Maggi chicken stock 20 g x 24 pieces',20.00,1580);
+INSERT INTO Products VALUES(8,'Goody light meat tuna in sun flower oil',8.25,809) ;
+INSERT INTO Products VALUES(9,'Goody Peanut Butter Creamy',16.50,6853);
+INSERT INTO Products VALUES(10,'Deemah date maamoul',24.95,25) ;
+INSERT INTO Products VALUES(11,'Goody black truffle mayonnaise',15.95,7) ;
+INSERT INTO Products VALUES(12,'Loacker biscuit tortina dark chocolate',14.95,0);
+INSERT INTO Products VALUES(13,'Almarai wild flowers natural honey',23.95,345);
+INSERT INTO Products VALUES(14,'Nutella with Hazelnut',19.77,66);
+INSERT INTO Products VALUES(15,'Cerelac with Wheat Nestlé Company',77.3,18) ;
+INSERT INTO Products VALUES(16,'Forno lays cheese',63.01,1);
+INSERT INTO Products VALUES(17,'Hershey’s milk chocolate chips',22.2,6) ;
+INSERT INTO Products VALUES(18,'Peanuts from Goody Company',17.5,9) ;
+INSERT INTO Products VALUES(19,'DUBBI with ketchup from Al BATAL',13.44,67) ;
+INSERT INTO Products VALUES(20,'Pringles Chips pepper flavor',44.5,44) ;
+INSERT INTO Products VALUES(21,'Brownies cake from Deemah company',43.21,89);
+INSERT INTO Products VALUES(22,'Cornflakes with oats from Nesquik company',67.41,65) ;
+INSERT INTO Products VALUES(23,'Break with chocolate from tiffany',55.43,78) ;
+INSERT INTO Products VALUES(24,'Cola candy with qummy candy from borgat',65.13,6) ;
+INSERT INTO Products VALUES(25,'Gardena with chocolate from loacker',55.43,8) ;
+
+INSERT INTO Repository VALUES('11223AA',23,1,1,'2020-10-1','1:22','7LCHaVyTgi9',2222) ;
+INSERT INTO Repository VALUES('11223AB',5,1,3,'2019-12-15','9:31','7LCHaVyTgi9',4444) ;
+INSERT INTO Repository VALUES('11221AA',2,1,2,'2021-1-5','4:12','7LCHaVyTgi9',2222);
+INSERT INTO Repository VALUES('11222AC',22,1,1,'2019-11-11','13:00','7LCHaVyTgi9',1111);
+INSERT INTO Repository VALUES('11222AX',12,0,3,'2020-10-7','9:30','41658469-76.119731',3333) ;
+INSERT INTO Repository VALUES('11223AV',1,1,2,'2021-1-20','1:23','36032091-117.161542',2222) ;
+INSERT INTO Repository VALUES('11221AM',14,1,2,'2020-10-10','5:55','FkJmqxy3U8',4444) ;
+INSERT INTO Repository VALUES('11223AC',3,1,2,'2020-1-5','22:38','7LCHaVyTgi9',8888);
+INSERT INTO Repository VALUES('11221AS',20,1,3,'2020-9-9','9:00','41658469-76.119731',7777);
+INSERT INTO Repository VALUES('11221AD',25,1,3,'2021-7-4','3:22','36032091-117.161542',7777) ;
+INSERT INTO Repository VALUES('11223AG',21,1,2,'2020-12-12','1:55','FkJmqxy3U8',7777) ;
+INSERT INTO Repository VALUES('11223AH',4,1,1,'2020-11-11','16:00','7LCHaVyTgi9',3333);
+INSERT INTO Repository VALUES('11222AW',6,1,3,'2021-10-7','8:30','41658469-76.119731',3333) ;
+INSERT INTO Repository VALUES('11222AT',7,1,1,'2021-1-20','11:23','36032091-117.161542',4444) ;
+INSERT INTO Repository VALUES('11221AQ',10,1,1,'2020-10-10','7:55','FkJmqxy3U8',4444) ;
+INSERT INTO Repository VALUES('11223AF',15,1,2,'2020-1-5','4:38','7LCHaVyTgi9',6666);
+INSERT INTO Repository VALUES('11221AK',9,1,'','2020-9-9','12:00','41658469-76.119731',6666);
+INSERT INTO Repository VALUES('11221AO',16,1,3,'2021-8-8','20:22','36032091-117.161542',6666) ;
+INSERT INTO Repository VALUES('11221AP',13,1,2,'2020-12-12','16:55','FkJmqxy3U8',6666) ;
+INSERT INTO Repository VALUES('11221AJ',18,1,2,'2020-12-11','14:55','FkJmqxy3U8',6666) ;
+INSERT INTO Repository VALUES('11223AU',19,1,3,'2020-7-1','11:55','FkJmqxy3U8',1111) ;
+INSERT INTO Repository VALUES('11222AQ',22,1,1,'2020-5-18','10:30','7LCHaVyTgi9',2222);
+INSERT INTO Repository VALUES('11222AL',12,1,2,'2020-6-13','12:45','36032091-117.161542',1118) ;
+INSERT INTO Repository VALUES('11223AL',11,1,3,'2020-9-19','11:45','36032091-117.161542',4444) ;
+INSERT INTO Repository VALUES('11222AZ',16,1,2,'2020-4-19','11:33','36032091-117.161542',7777) ;
+INSERT INTO Repository VALUES('11222AR',10,1,3,'2020-4-11','12:33','41658469-76.119731',5555) ;
+INSERT INTO Repository VALUES('11222AG',9,1,2,'2020-1-11','9:33','FkJmqxy3U8',6666);
+INSERT INTO Repository VALUES('11221AI',8,1,2,'2020-2-11','11:00','FkJmqxy3U8',3333);
+INSERT INTO Repository VALUES('11221AE',5,1,2,'2020-8-11','11:20','36032091-117.161542',8888) ;
+INSERT INTO Repository (Reponumber) VALUES('11221AZ') ;
+
+INSERT INTO Purchases VALUES ('999005033', 1230,1,1002,'11223AA') ;
+INSERT INTO Purchases VALUES ('999115033', 1232,2,2468,'11223AB');
+INSERT INTO Purchases VALUES ('999225033', 1235,7,1000,'11223AV');
+INSERT INTO Purchases VALUES ('999335033', 1234,13,145,NULL) ;
+INSERT INTO Purchases VALUES ('999445033', 1233,10,25,'11223AC') ;
+INSERT INTO Purchases VALUES ('999555033', 1237,17,3,'11222AW') ;
+INSERT INTO Purchases VALUES ('999665033', 1234,13,200,'11221AQ');
+INSERT INTO Purchases VALUES ('999775033', 12311,18,3,'11221AP') ;
+INSERT INTO Purchases VALUES ('999885033', 12310,20,44,NULL) ;
+INSERT INTO Purchases VALUES ('999995033', 1238,21,89,NULL) ;
+INSERT INTO Purchases VALUES ('999915033', 1235,7,580,'11222AL') ;
+INSERT INTO Purchases VALUES ('999925033', 12311,18,6,'11221AE') ;
+INSERT INTO Purchases VALUES ('999935033', 1237,17,3,'11222AG');
+
+INSERT INTO PurchasesInvoices VALUES ('999005033',16.15,'2013-07-23 08:12:45.132748');
+INSERT INTO PurchasesInvoices VALUES ('999115033',16.15,'2013-11-01 13:05:44.321654');
+INSERT INTO PurchasesInvoices VALUES ('999225033',4.30,'2016-09-22 09:12:21.789456');
+INSERT INTO PurchasesInvoices VALUES ('999335033',17.33,'2017-09-12 09:12:21.789456');
+INSERT INTO PurchasesInvoices VALUES ('999445033',20.03,'2017-08-11 11:16:01.543567');
+INSERT INTO PurchasesInvoices VALUES ('999555033',19.02,'2018-12-24 05:33:22.321456');
+INSERT INTO PurchasesInvoices VALUES ('999665033',17.33,'2018-12-25 08:33:44.321987');
+INSERT INTO PurchasesInvoices VALUES ('999775033',13.3,'2019-02-02 07:33:22.947386');
+INSERT INTO PurchasesInvoices VALUES ('999885033', 35.55,'2020-12-04 11:22:22.564789');
+INSERT INTO PurchasesInvoices VALUES ('999995033',39.09,'2021-11-11 09:22:11.987345');
+INSERT INTO PurchasesInvoices VALUES ('999915033',4.30,'2021-11-11 08:22:11.987345');
+INSERT INTO PurchasesInvoices VALUES ('999925033',13.3,'2021-11-11 13:22:11.987345');
+INSERT INTO PurchasesInvoices VALUES ('999935033',19.02,'2021-11-11 12:22:11.987345');
+
+INSERT INTO Sales VALUES ('988000123',1,23,1);
+INSERT INTO Sales VALUES ('988000113',3,5,5);
+INSERT INTO Sales VALUES ('988000122',2,2,12);
+
+INSERT INTO Sales VALUES ('988000133',1,22,1);
+INSERT INTO Sales VALUES ('988000143',3,12,3);
+INSERT INTO Sales VALUES ('988000144',2,1,2);
+INSERT INTO Sales VALUES ('988000155',2,14,4);
+INSERT INTO Sales VALUES ('988000166',2,3,6);
+INSERT INTO Sales VALUES ('988000161',3,20,1);
+INSERT INTO Sales VALUES ('988000167',3,25,1);
+
+INSERT INTO SalesInvoices VALUES ('988000123',55.43,'2021-01-11 09:22:11.287345');
+INSERT INTO SalesInvoices VALUES ('988000113',111.9,'2021-01-12 10:22:11.287344');
+INSERT INTO SalesInvoices VALUES ('988000122',268.56,'2021-02-11 11:24:11.385545');
+INSERT INTO SalesInvoices VALUES ('988000133',67.41,'2021-04-25 16:26:31.987345');
+INSERT INTO SalesInvoices VALUES ('988000143',44.85,'2021-05-11 14:22:11.487345');
+INSERT INTO SalesInvoices VALUES ('988000144',44.76,'2021-05-19 10:22:51.987345');
+INSERT INTO SalesInvoices VALUES ('988000155',59.08,'2021-06-11 09:22:11.557345');
+INSERT INTO SalesInvoices VALUES ('988000166',134.28,'2021-08-11 12:44:11.787945');
+INSERT INTO SalesInvoices VALUES ('988000161',44.5,'2021-09-11 09:22:17.788845');
+INSERT INTO SalesInvoices VALUES ('988000167',55.43,'2021-10-21 14:32:15.187345');
